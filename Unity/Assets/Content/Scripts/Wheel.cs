13ea416a6e310cf015c62c06adf71692c77d1af6
PY Boulerice <pboulerice@sagosago.com>
2018-05-14 11:25:08 -0400

    Clean up wheel script for presentation

--- Unity/Assets/Content/Scripts/Wheel.cs
+++ Unity/Assets/Content/Scripts/Wheel.cs
@@ -1,180 +1,188 @@
 ﻿namespace Mechanic {
 
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
-    using SagoTouch;
-    using Touch = SagoTouch.Touch;
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using SagoTouch;
+	using Touch = SagoTouch.Touch;
 
-    /// <summary>
-    /// Handles a wheel that can be picked up and bounces on surfaces
-    /// </summary>
-    public class Wheel : MonoBehaviour, ISingleTouchObserver {
+	/// <summary>
+	/// Handles a wheel that can be picked up and bounces on surfaces
+	/// </summary>
+	public class Wheel : MonoBehaviour, ISingleTouchObserver {
 
-        #region Properties
+		#region Fields
 
-        // Visible
-        [SerializeField]
-        private Camera Camera;
+		// Visible
+		[SerializeField]
+		private Camera Camera;
 
-        [SerializeField]
-        private Renderer Renderer;
+		[SerializeField]
+		private Renderer Renderer;
 
-        [Space]
-        [Header("Animation")]
-        [SerializeField]
-        [Range(1, 20)]
-        private float respawnAnimationSpeed;
-        [SerializeField]
-        private AnimationCurve respawnAnimationCurve;
+		[Space]
+		[Header("Animation")]
+		[SerializeField]
+		[Range(1, 20)]
+		private float respawnAnimationSpeed;
+		[SerializeField]
+		private AnimationCurve respawnAnimationCurve;
 
-        [Space]
-        [Header("Interaction")]
-        [SerializeField]
-        private float draggingSpeed;
-        
-        // ---
+		[Space]
+		[Header("Interaction")]
+		[SerializeField]
+		private float draggingSpeed;
 
-        // Defaults
-        private PhysicsMaterial2D defaultPhysicsMaterial;
-        private Vector2 defaultPosition;
+		// ---
 
-        private Touch Touch;
+		// Defaults
+		private PhysicsMaterial2D defaultPhysicsMaterial;
+		private Vector2 defaultPosition;
 
-        // Components
-        private Rigidbody2D rb2d;
+		private Touch Touch;
 
-        // States
-        private enum ObjectState {
-            Active,
-            Respawning,
-            Inactive
-        }
+		// Components
+		private Rigidbody2D rb2d;
 
-        private ObjectState state;
+		// States
+		private enum ObjectState {
+			Active,
+			Respawning,
+			Inactive
+		}
 
-        #endregion
+		private ObjectState state;
 
-        // ---
+		#endregion
 
-        // Unity Defaults
+		// ---
 
-        private void Awake() {
-            rb2d = GetComponent<Rigidbody2D>();
-            defaultPhysicsMaterial = rb2d.sharedMaterial;
-            defaultPosition = transform.position;
-        }
+		#region Unity Defaults
 
-        private void OnEnable() {
+		private void Awake() {
+			rb2d = GetComponent<Rigidbody2D>();
+			defaultPhysicsMaterial = rb2d.sharedMaterial;
+			defaultPosition = transform.position;
+		}
 
-            TouchDispatcher.Instance.Add(this, 0);
-        }
+		private void OnEnable() {
 
-        private void OnDisable() {
+			TouchDispatcher.Instance.Add(this, 0);
+		}
 
-            TouchDispatcher.Instance.Remove(this);
-        }
+		private void OnDisable() {
 
-        private void FixedUpdate() {
-            if (state == ObjectState.Active) {
-                DefaultDragging();
-                CheckBounds();
-            }
-        }
+			TouchDispatcher.Instance.Remove(this);
+		}
 
+		private void FixedUpdate() {
+			
+			if (state == ObjectState.Active) {
+				DefaultDragging();
+				CheckBounds();
+			}
+		}
 
-        // Dragging Functionality
+		#endregion
 
-        private void DefaultDragging() {
-            if (this.Touch != null) {
+		// Dragging Functionality
 
-                Vector2 currentVelocity;
-                Vector2 currentPosition = transform.position;
-                Vector2 touchPosition = Camera.ScreenToWorldPoint(this.Touch.Position);
+		private void DefaultDragging() {
+			
+			if (this.Touch != null) {
 
-                Vector2 targetPosition = touchPosition - currentPosition;
+				Vector2 currentVelocity;
+				Vector2 currentPosition = transform.position;
+				Vector2 touchPosition = Camera.ScreenToWorldPoint(this.Touch.Position);
 
-                currentVelocity = targetPosition * draggingSpeed / Time.fixedDeltaTime;
+				Vector2 targetPosition = touchPosition - currentPosition;
 
-                Vector2 targetVelocity = currentVelocity - rb2d.velocity;
+				currentVelocity = targetPosition * draggingSpeed / Time.fixedDeltaTime;
 
-                Vector2 force = rb2d.mass * targetVelocity / Time.fixedDeltaTime;
+				Vector2 targetVelocity = currentVelocity - rb2d.velocity;
 
-                rb2d.AddForce(force);
-            }
-        }
+				Vector2 force = rb2d.mass * targetVelocity / Time.fixedDeltaTime;
 
-
-        // Out of bounds Functionality
-
-        private void CheckBounds() {
-            if (!this.Renderer.isVisible) {
-                state = ObjectState.Respawning;
-                StartCoroutine(Respawning());
-            }
-        }
-
-        private IEnumerator Respawning() {
-            float timeElapsed = 0;
-
-            // ---
-
-            transform.localScale = Vector3.zero;
-
-            yield return new WaitForSeconds(1);
-
-            // Reset velocity
-            rb2d.isKinematic = true;
-            rb2d.velocity = Vector2.zero;
-            rb2d.angularVelocity = 0;
-            transform.position = defaultPosition;
-
-            while (timeElapsed <= 1) {
-                timeElapsed += Time.deltaTime * respawnAnimationSpeed;
-
-                transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, respawnAnimationCurve.Evaluate(timeElapsed));
-                yield return 0;
-            }
-
-            rb2d.isKinematic = false;
-            state = ObjectState.Active;
-        }
+				rb2d.AddForce(force);
+			}
+		}
 
 
-        // ISingleTouchObserver Implementation
+		// Out of bounds Functionality
 
-        public bool OnTouchBegan(Touch touch) {
+		private void CheckBounds() {
+			
+			if (!this.Renderer.isVisible) {
+				state = ObjectState.Respawning;
+				StartCoroutine(Respawning());
+			}
+		}
 
-            // Actually check if the touch is within this object
-            RaycastHit2D hit2D;
-            Vector2 ray = Camera.ScreenToWorldPoint(touch.Position);
+		private IEnumerator Respawning() {
+			
+			float timeElapsed = 0;
 
-            hit2D = Physics2D.Raycast(ray, Vector3.back);
+			// ---
 
-            if (hit2D.transform == transform) {
-                rb2d.sharedMaterial = null;
-                this.Touch = touch;
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
+			transform.localScale = Vector3.zero;
 
-        public void OnTouchMoved(Touch touch) {
+			yield return new WaitForSeconds(1);
 
-        }
+			// Reset velocity
+			rb2d.isKinematic = true;
+			rb2d.velocity = Vector2.zero;
+			rb2d.angularVelocity = 0;
+			transform.position = defaultPosition;
 
-        public void OnTouchEnded(Touch touch) {
-            this.Touch = null;
-            rb2d.sharedMaterial = defaultPhysicsMaterial;
-        }
+			while (timeElapsed <= 1) {
+				timeElapsed += Time.deltaTime * respawnAnimationSpeed;
 
-        public void OnTouchCancelled(Touch touch) {
+				transform.localScale = Vector3.Lerp(Vector3.zero, Vector3.one, respawnAnimationCurve.Evaluate(timeElapsed));
+				yield return 0;
+			}
 
-        }
+			rb2d.isKinematic = false;
+			state = ObjectState.Active;
+		}
+
+		#region Touch
+
+		// ISingleTouchObserver Implementation
+
+		public bool OnTouchBegan(Touch touch) {
+
+			// Actually check if the touch is within this object
+			RaycastHit2D hit2D;
+			Vector2 ray = Camera.ScreenToWorldPoint(touch.Position);
+
+			hit2D = Physics2D.Raycast(ray, Vector3.back);
+
+			if (hit2D.transform == transform) {
+				rb2d.sharedMaterial = null;
+				this.Touch = touch;
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+
+		public void OnTouchMoved(Touch touch) {
+
+		}
+
+		public void OnTouchEnded(Touch touch) {
+			this.Touch = null;
+			rb2d.sharedMaterial = defaultPhysicsMaterial;
+		}
+
+		public void OnTouchCancelled(Touch touch) {
+
+		}
+
+		#endregion
 
 
-    }
+	}
 
 }

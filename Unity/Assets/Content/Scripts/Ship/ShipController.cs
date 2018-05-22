2534f1a639bca138e7356221f0df10dbfed8d7b4
PY Boulerice <pboulerice@sagosago.com>
2018-05-22 14:10:32 -0400

    Updated ShipController to have a scale down coroutine

--- Unity/Assets/Content/Scripts/Ship/ShipController.cs
+++ Unity/Assets/Content/Scripts/Ship/ShipController.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	private Rigidbody2D rigidbody2D;
+
+	private void Awake() {
+		rigidbody2D = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void Thrust(Vector2 force)
+	{
+		rigidbody2D.AddForce(force);
 	}
+
+	public void Rotate(float torque)
+	{
+		rigidbody2D.AddTorque(torque);
+	}
+
+	public IEnumerator ScaleDown() {
+        float timeElapsed = 0;
+        while (timeElapsed <= 1) {
+
+			timeElapsed += Time.deltaTime;
+
+			transform.localScale = Vector2.Lerp(Vector2.one, Vector2.zero, timeElapsed);
+
+            yield return 0;
+        }
+    }
 }

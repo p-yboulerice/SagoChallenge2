f1b2e8731325747f412cf5f5a57f0fb598bf3bdc
PY Boulerice <pboulerice@sagosago.com>
2018-05-22 13:42:25 -0400

    Created ship spawn action

---
+++ Unity/Assets/Content/Scripts/Ship/Actions/SA_SpawnShip.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Juice.FSM;
+
+public class SA_SpawnShip : StateAction {
+
+	private ShipManager m_ShipManager;
+
+	private ShipManager ShipManager {
+		get { return m_ShipManager = m_ShipManager ?? this.GetComponentInParent<ShipManager>(); } 
+	}
+
+	public override void Run() {
+		this.ShipManager.SpawnShip();
+	}
+
+}
+     

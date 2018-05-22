4b267b612f0e9c748defe3a9e5e63877fe0d8bff
PY Boulerice <pboulerice@sagosago.com>
2018-05-22 14:09:33 -0400

    Created Explosion scene action script

---
+++ Unity/Assets/Content/Scripts/Ship/Actions/SA_Explosion.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Juice.FSM;
+
+public class SA_Explosion : StateAction {
+
+	#region Properties
+
+	private ShipManager m_ShipManager;
+
+	private ShipManager ShipManager {
+		get { return m_ShipManager = m_ShipManager ?? this.GetComponentInParent<ShipManager>(); }
+	}
+
+	private ShipController m_ShipController;
+
+	private ShipController ShipController {
+		get { return m_ShipController = m_ShipController ?? this.GetComponentInParent<ShipController>(); }
+	}
+
+	#endregion
+
+	#region Fields
+
+	[SerializeField]
+	private GameObject explosionPrefab;
+
+	#endregion
+
+	#region Methods
+
+	public override void Run() {
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+		StartCoroutine(Explosion());
+    }
+
+    private IEnumerator Explosion() {
+        yield return this.ShipController.ScaleDown();
+		this.ShipManager.RemoveShip(this.ShipController);      
+    }
+
+	#endregion   
+
+}

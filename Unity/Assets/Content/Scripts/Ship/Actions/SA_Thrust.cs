0ec2b677d9bf9cb7173b6b9782bbc7183cb3bba6
PY Boulerice <pboulerice@sagosago.com>
2018-05-22 15:08:54 -0400

    Created thrust and torque state action scripts

---
+++ Unity/Assets/Content/Scripts/Ship/Actions/SA_Thrust.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Juice.FSM;
+
+public class SA_Thrust : StateAction {
+
+	private ShipManager shipManager;
+
+	private void OnEnable() {
+		shipManager = GetComponentInParent<ShipManager>();
+	}
+
+	public override void Run() {
+		shipManager.ThrustShips();
+	}
+
+}
---
+++ Unity/Assets/Content/Scripts/Ship/Actions/SA_Torque.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Juice.FSM;
+
+public class SA_Torque : StateAction {
+
+	private ShipManager shipManager;
+
+	private enum Direction {
+		Right = -1,
+        Left = 1
+	}
+
+	[SerializeField]
+	private Direction direction;
+    
+    private void OnEnable() {
+        shipManager = GetComponentInParent<ShipManager>();
+    }
+
+	public override void Run() {
+		shipManager.RotateShips((int)direction);
+	}
+
+}

ef306f6c72646c15d62d3a134ddf4cef0dbdcee5
PY Boulerice <pboulerice@sagosago.com>
2018-05-23 10:31:29 -0400

    Added Collision State Flag and Velocity State Flag

---
+++ Unity/Assets/Content/Scripts/Ship/Flags/SF_Collision.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Juice.FSM;
+
+public class SF_Collision : StateFlag {
+
+	#region Properties
+
+	[SerializeField]
+	private Collider2D m_Collider;
+
+	private Collider2D Collider {
+		get { return m_Collider = m_Collider ?? GetComponentInParent<PolygonCollider2D>(); }
+	}
+
+	#endregion
+
+	#region Fields
+
+	[SerializeField]
+	private ContactFilter2D filter;
+
+	private Collider2D[] collisions = new Collider2D[4];
+
+	#endregion
+
+	#region Properties
+
+	public override bool IsActive {
+		get {
+			Physics2D.OverlapCollider(this.Collider, filter, collisions);
+
+			for (int i = 0; i < collisions.Length; i++) {
+				if (collisions[i] != null) {
+					collisions[i].GetComponent<ShipController>();
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+
+	#endregion
+
+}
---
+++ Unity/Assets/Content/Scripts/Ship/Flags/SF_VelocityExceeds.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Juice.FSM;
+
+public class SF_VelocityExceeds : StateFlag {
+
+	#region Properties
+
+	private Rigidbody2D m_Rigidbody2D;
+
+	private Rigidbody2D Rigidbody2D {
+		get { return m_Rigidbody2D = m_Rigidbody2D ?? this.GetComponentInParent<Rigidbody2D>(); }
+	}
+
+	#endregion
+
+	#region Fields
+
+	[SerializeField]
+	private float velocityThreshold;
+
+	#endregion
+
+	#region Methods
+
+	public override bool IsActive {
+		get {
+			if (this.Rigidbody2D.velocity.magnitude > velocityThreshold) {
+				return true;
+			}
+
+			return false;
+		}
+	}
+
+	#endregion
+
+}
